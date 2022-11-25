b076662fabc70a1755a3f39b8c159abd30f42eec
NaitsirhcEmorej07 <naitsirhcemorej@gmail.com>
2022-11-25 09:30:45 +0800

    nav menu justify content right and add padding

--- nav.php
+++ nav.php
@@ -10,11 +10,11 @@
     $active_menu_link = 'active'; // For settings to be dynamic
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Covid19 Vaccinated Records</a>
+  <a class="navbar-brand px-4" href="#">Covid19 Vaccinated Records</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarText">
+  <div class="collapse navbar-collapse justify-content-end px-4" id="navbarText">
     <ul class="navbar-nav">
       <li class="nav-item <?php echo $active_menu_link; ?>">
         <a class="nav-link" href="index.php">Dashboard</a>
